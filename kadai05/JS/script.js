18445e0c4ff0f730ad795b03c80a6478b88013f8
hina0629 <hina.0629.xxx@gmail.com>
2025-07-02 17:08:12 +0900

    Enterをおして検索のJS

--- kadai05/JS/script.js
+++ kadai05/JS/script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
     /* activeクラスを付け外し */
     menu.classList.toggle("active");
 }
+
+/* 検索窓Enterを押して検索 */
+document.getElementById("searchInput").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        /* ページのリロード防止 */
+        event.preventDefault();
+        const keyword = event.target.value;
+        /* ここに検索する関数名をかく */
+        console.log(keyword)
+    }
+})
