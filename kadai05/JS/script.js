3d24d78e89292c65cca3ffca85e94c33e87c810f
hina0629 <hina.0629.xxx@gmail.com>
2025-07-03 17:23:19 +0900

    ランダム

--- kadai05/JS/script.js
+++ kadai05/JS/script.js
@@ -5,9 +5,18 @@ const icon = togglebutton.querySelector("i");
 
 togglebutton.addEventListener("click", function () {
     menu.classList.toggle("open");
-    icon.classList.toggle("fa-bars");
-    icon.classList.toggle("fa-xmark");
-})
+    
+    icon.style.opacity = "0";
+    icon.style.transform = "scale(0.8)";
+
+    setTimeout( function () {
+        icon.classList.toggle("fa-bars");
+        icon.classList.toggle("fa-xmark");
+
+        icon.style.opacity = "1";
+        icon.style.transform = "scale(1)";
+    }, 200);
+});
 
 /* 検索窓Enterを押して検索 */
 document.getElementById("searchInput").addEventListener("keydown", function(event) {
@@ -19,3 +28,19 @@ document.getElementById("searchInput").addEventListener("keydown", function(even
         console.log(keyword)
     }
 })
+
+/* ランダム表示 */
+const random = document.getElementById("random");
+
+function createPokemonCard(paddedNumber) {
+    return `
+    <img src="./img/${paddedNumber}.png" alt="">
+    `
+}
+
+for (let i = 0; i < 5; i++) {
+    let No = Math.floor(Math.random() * 151) + 1;
+    /* ゼロパディング */
+    const paddedNumber = String(No).padStart(3, '0');
+    random.innerHTML = createPokemonCard(paddedNumber);
+}
