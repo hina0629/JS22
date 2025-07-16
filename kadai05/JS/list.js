565e9cc0c1a86abc519ae5c91b20d7806ab91653
hina0629 <hina.0629.xxx@gmail.com>
2025-07-16 17:12:07 +0900

    ポケモン図鑑のリンクテキストを変更し、フッターの著作権表記を修正

--- kadai05/JS/list.js
+++ kadai05/JS/list.js
@@ -109,42 +109,4 @@ clearButton.addEventListener('click', function () {
 // 初期ロード
 loadAllPokemon();
 
-// for (let i = 1; i <= 151; i++) {
-//     // ポケモンの番号をゼロパディング
-//     const listPaddedNumber = String(i).padStart(3, '0');
-
-//     const pokemonListCard = document.createElement('div');
-//     pokemonListCard.classList.add('pokemon-list-card');
-
-//     const pokemonListImage = document.createElement('img');
-//     pokemonListImage.src = `./img/${listPaddedNumber}.png`;
-//     pokemonListImage.alt = `ポケモンNo.${listPaddedNumber}`;
-//     console.log(`No.${listPaddedNumber}`);
-
-//     // マウスオーバーでGIFに変更
-//     pokemonListImage.addEventListener("mouseover", function () {
-//         pokemonListImage.src = `./img/${listPaddedNumber}gif.png`;
-//     });
-
-//     // 元の画像に戻す
-//     pokemonListImage.addEventListener("mouseout", function () {
-//         pokemonListImage.src = `./img/${listPaddedNumber}.png`;
-//     });
-
-    
-//     // 日本語名の取得
-//     fetch(`https://pokeapi.co/api/v2/pokemon-species/${i}`)
-//     .then(response => response.json())
-//     .then(data => {
-//     const japaneseName = data.names.find(name => name.language.name === 'ja');
-//     const name = japaneseName ? japaneseName.name : '名前不明';
-
-//     const nameElement = document.createElement('p');
-//     nameElement.textContent = `${name}`;
-//     nameElement.classList.add('dotgothic16-regular');
-//     pokemonListCard.appendChild(nameElement);
-//     });
-
-//     pokemonListCard.appendChild(pokemonListImage);
-//     pokemonList.appendChild(pokemonListCard);
-// }
+// 検索
