c8230266e3c6ce71cfb5a4ee5ddded2ffe46138b
hina0629 <hina.0629.xxx@gmail.com>
2025-09-10 15:55:32 +0900

    TODO: Cookieに保存: THEME_KEY, theme

--- 0910/36_cookie/basic/js/theme.js
+++ 0910/36_cookie/basic/js/theme.js
@@ -22,6 +22,7 @@ function applyTheme(value) {
     if (icon) icon.textContent = theme === "dark" ? "🌙" : "🌞";
 
     // TODO: Cookieに保存: THEME_KEY, theme
+    setCookie(THEME_KEY, theme);
 
     // ボタンを更新: UI上だけでなくアクセシビリティ的にも明示するための属性
     document.getElementById("themeBtn")?.setAttribute("aria-pressed", String(theme === "dark"));
